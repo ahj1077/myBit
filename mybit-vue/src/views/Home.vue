ec76e54d6600bddf75096eee96c26cf751987795
안형준 <ahj1077@gmail.com>
2021-10-03 01:50:57 +0900

    전일대비 고가 저가 추가

--- mybit-vue/src/views/Home.vue
+++ mybit-vue/src/views/Home.vue
@@ -9,7 +9,39 @@
                 </div>         
             </div>
             <div id="coin_price">
-                <div id="price_now"><h2 style="font-weight : bold;">{{selectedCoinData.currentPrice}}</h2> KRW</div>
+              <span id="coin_price_left" v-bind:style="textColor">
+                <div id="price_now" ><h2 style="font-weight : bold;">{{selectedCoinData.currentPrice}}</h2> KRW</div>
+                <span v-bind:style="grayColor"><span style="font-weight : bold; font-size:13px">전일대비</span></span> <span style="font-weight : bold;">{{selectedCoinData.signed_change_rate}}% </span>
+                <span style="font-weight : bold;">
+                  <span v-if="selectedCoinData.signed_change_rate.startsWith('+')">▲</span>
+                  <span v-if="selectedCoinData.signed_change_rate.startsWith('-')">▼</span>
+                  <span>{{selectedCoinData.signed_change_price}}</span>
+                </span>
+              </span>
+
+              <span id="coin_price_mid">
+                <div style="display:flex; height:50%; border-bottom:1px solid #333333; padding-bottom : 1%;">
+                  <span class="tbx_gray" style="padding-top:3%">고가</span>
+                  <span style="width:80%; padding-top:3%; text-align:right; font-weight : bold;" v-bind:style="redColor">{{selectedCoinData.high_price}}</span>
+                </div>
+                <div style="display:flex; height:50%; padding-top : 3%; padding-bottom : 3%;">
+                  <span class="tbx_gray" style="padding-top:3%">저가</span>
+                  <span style="width:80%; padding-top:3%; text-align:right; font-weight : bold;" v-bind:style="blueColor">{{selectedCoinData.low_price}}</span>
+                </div>                
+              </span>
+
+              <span id="coin_price_right">
+                 <div style="display:flex; height:50%; border-bottom:1px solid #333333; padding-bottom : 3%;">
+                  <span class="tbx_gray" style="width:50%; text-align:left; padding-top:3%;">거래량(24H)</span>
+                  <span style="width:50%; text-align:right; padding-top:3%; padding-right:2%; font-size:14px; font-weight:1;">{{selectedCoinData.acc_trade_volume_24h}}</span>
+                  <span class="tbx_gray" style="padding-top : 3%;">{{selectedCoinData.marketCode.split('-')[1]}}</span>
+                </div>
+                 <div style="display:flex; height:50%; padding-top : 3%; padding-bottom : 3%;">
+                  <span class="tbx_gray" style="width:50%; text-align:left; padding-top:3%;">거래대금(24H)</span>
+                  <span style="width:50%; padding-top:3%; text-align:right; padding-right:2%; font-size:14px; font-weight:1;">{{selectedCoinData.acc_trade_price_24h}}</span>
+                  <span class="tbx_gray" style="padding-top : 3%;">{{selectedCoinData.marketCode.split('-')[0]}}</span>
+                </div>
+              </span>
             </div>
             <div id="chart">
                 <apexcharts type="candlestick" :options="chartOptions" :series="series"></apexcharts>
@@ -34,11 +66,23 @@
                         <th class="right_align">전일대비</th>
                         <th class="right_align">거래대금</th>
                     </tr>
-                    <tr class="coin_list_content" v-for="coin in coins_list_data" v-bind:key="coin.market">                        
-                        <td class="left_align" style="text-align: left; color: #FFFFFF;">{{coin.korean_name}}</td>
+                    <tr class="coin_list_content" v-for="coin in coins_list_data" v-bind:key="coin.market"
+                      v-bind:class="{
+                          text_red : coin.signed_change_rate.startsWith('+'),
+                          text_blue : coin.signed_change_rate.startsWith('-'),
+                          text_white : coin.signed_change_rate.startsWith('+') == false && coin.signed_change_rate.startsWith('-') == false,
+                          selected_color : coin.market == selectedCoinData.marketCode                           
+                        }"                                                              
+                      v-on:click="coinItem_onclick(coin)">
+
+                        <td class="left_align" style="text-align: left; color: white;">{{coin.korean_name}}
+                          <div style="font-weight : normal; color: #AAAAAA; font-size : 11px;">{{coin.market.split('-')[1] + '/' + coin.market.split('-')[0]}}</div>                        
+                        </td>
                         <td class="right_align">{{coin.trade_price}}</td>
-                        <td class="right_align">{{coin.signed_change_rate}}%</td>
-                        <td class="right_align" style="color: #CCCCCC;">{{coin.acc_trade_price_24h}}백만</td>
+                        <td class="right_align" style="font-weight : normal">{{coin.signed_change_rate}}%
+                          <div>{{coin.signed_change_price}}</div>
+                        </td>
+                        <td class="right_align" v-bind:style="{color:'#CCCCCC'}">{{coin.acc_trade_price_24h}}<span  v-bind:style="grayColor">백만</span></td>
                     </tr>
                 </table>
             </div>
@@ -103,8 +147,22 @@
         color: #FFFFFF;
     }
 
-    #coin_price {
-        padding: 1%;
+    #coin_price {      
+      padding: 1.5% 1.5% 1% 1.5%;
+      border-bottom: 1px solid #333333;
+      display: flex;
+    }
+
+    #coin_price_left {      
+      width: 50%;
+    }
+
+    #coin_price_mid {           
+      width: 25%;
+    }
+
+    #coin_price_right {      
+      width: 25%;
     }
 
     #price_now h2{
@@ -190,22 +248,22 @@
         height: 1000px;
         width: 100%;
         overflow: auto;
+        
     }
 
     #coin_list_contents table {
         width: 100%;
     }
 
-    #coin_list_contents table tr{
-        
+    #coin_list_contents table tr{        
         width: 100%;
         border-bottom: 1px solid #333333;
     }
 
-    #coin_list_col_header {
-        
+    #coin_list_col_header {        
         position: sticky;
         top: 0px;
+        height: 30px;
         align-items: center;
         color: #666666;
         background-color: #1A2436;
@@ -229,8 +287,7 @@
 
     .coin_list_content td{
         font-size: 13px;
-        font-weight: bold;
-        background-color: #1A2436;
+        font-weight: bold;        
         height: 100%;
         width: 25%;
     }
@@ -238,6 +295,10 @@
     .coin_list_content .left_align {
         text-align: left;
         padding-left: 10px;
+    }
+
+    .coin_list_content:hover {
+      background-color : #1F2A3C;
     }
 
     .right_align {
@@ -272,6 +333,14 @@
       color: #FFFFFF;
     }
 
+    .selected_color {
+      background-color: #1F2A3C;
+    }
+
+    .tbx_gray {
+      color: #AAAAAA;
+      font-size: 13px;
+    }
 
 </style>
 
@@ -372,8 +441,19 @@ var data =  {
     korean_name : '비트코인',
     currentPrice : 0,
     signed_change_rate : 0,
+    signed_change_price : 0,
+    high_price : 0,
+    low_price : 0,
+    acc_trade_price_24h : 0,
+    acc_trade_volume_24h : 0,
   },
   unit : 1,
+  textColor : {
+      color: '',      
+    },
+  redColor: { color : '#B84042'},
+  blueColor: { color : '#0C66C6'},
+  grayColor: { color : '#666666'},
 }
 
 function makeMarketCodeList_KRW(data){
@@ -408,24 +488,32 @@ export default {
       }, 300);
 
     },
+    watched : {
+        
+    },
     methods : {
-
+      /** 
+       *  코인 리스트를 가져옴
+       */
       retrieve_coin_list_info : function(){
                         
-                        axios.get('http://localhost:8080/marketCode?isDetails=true')
-                        .then((result) => {
-                            data.coins = result.data;  
+        axios.get('http://localhost:8080/marketCode?isDetails=true')
+        .then((result) => {
+            data.coins = result.data;  
 
-                            var marketCodeList_KRW = makeMarketCodeList_KRW(result.data);
+            var marketCodeList_KRW = makeMarketCodeList_KRW(result.data);
 
-                            axios.get('http://localhost:8080/currentCoinInfo', {params: { marketCode : marketCodeList_KRW }})
-                                .then((result) => {
-                                    
-                                    this.setCoinsListData(result.data);                                    
-                                })         
-                        })                 
-                      },
-
+            axios.get('http://localhost:8080/currentCoinInfo', {params: { marketCode : marketCodeList_KRW }})
+                .then((result) => {
+                    
+                    this.setCoinsListData(result.data);                                    
+                })         
+        })                 
+      },
+      /**
+       * 
+       * 코인 리스트 데이터 세팅
+       */
       setCoinsListData : function(data){
           this.coins_list_data = [];
 
@@ -433,15 +521,57 @@ export default {
             for(var j=0; j<data.length; j++){
               if(this.coins[i].market == data[j].market) {
 
-                  data[j].trade_price = Math.floor(data[j].trade_price).toLocaleString('ko-KR');
+                   //현재가에 소수점 이하가 존재하는경우 4자리까지
+                  if(data[j].trade_price % 1 > 0){
+                    data[j].trade_price = data[j].trade_price.toLocaleString('ko-KR');
+                  }
+                  else{
+                    data[j].trade_price = Math.floor(data[j].trade_price).toLocaleString('ko-KR');
+                  }
                   data[j].signed_change_rate =  (data[j].signed_change_rate * 100).toFixed(2);
                   if(data[j].signed_change_rate > 0) data[j].signed_change_rate = "+"+String(data[j].signed_change_rate);
-                  data[j].acc_trade_price_24h = Math.floor(data[j].acc_trade_price_24h / 1000000);
+                  
+                  // 전일대비 증감액이 2자릿수 이하면 소수점 둘째자리까지 표시
+                  if(Math.floor(data[j].signed_change_price / 100) >0)
+                    data[j].signed_change_price = Math.floor(data[j].signed_change_price).toLocaleString('ko-KR');
+                  else
+                    data[j].signed_change_price = parseFloat(data[j].signed_change_price).toFixed(2).toLocaleString('ko-KR');
+                  
+                  var selectedCoinAccTragePrice24h = Math.floor(data[j].acc_trade_price_24h).toLocaleString('ko-KR');
+                  data[j].acc_trade_price_24h =  Math.floor(data[j].acc_trade_price_24h / 1000000).toLocaleString('ko-KR');   //거래대금
+                  data[j].acc_trade_volume_24h = parseFloat(parseFloat(data[j].acc_trade_volume_24h).toFixed(3)).toLocaleString('ko-KR'); //거래량
 
+                  if(Math.floor(data[j].high_price / 100) >0)
+                    data[j].high_price = Math.floor(data[j].high_price).toLocaleString('ko-KR');
+                  else
+                    data[j].high_price = Math.floor(data[j].high_price).toFixed(2).toLocaleString('ko-KR');
+                  
+                  if(Math.floor(data[j].low_price / 100) >0)
+                    data[j].low_price = Math.floor(data[j].low_price).toLocaleString('ko-KR');
+                  else
+                    data[j].low_price = Math.floor(data[j].low_price).toFixed(2).toLocaleString('ko-KR');                                      
+
+                  // 현재 선택된 코인데이터 세팅
                   if(data[j].market == this.selectedCoinData.marketCode){
                     this.selectedCoinData.korean_name = this.coins[i].korean_name;
                     this.selectedCoinData.currentPrice = data[j].trade_price;
                     this.selectedCoinData.signed_change_rate = data[j].signed_change_rate;
+                    this.selectedCoinData.signed_change_price = data[j].signed_change_price;
+                    this.selectedCoinData.high_price = data[j].high_price;
+                    this.selectedCoinData.low_price = data[j].low_price;  
+                    this.selectedCoinData.acc_trade_price_24h = selectedCoinAccTragePrice24h;
+                    this.selectedCoinData.acc_trade_volume_24h = data[j].acc_trade_volume_24h;
+
+                    //현재 선택된 코인 증감률에따라 색상 변경
+                    if(this.selectedCoinData.signed_change_rate.startsWith('+')){
+                      this.textColor.color = this.redColor.color;
+                    }
+                    else if(this.selectedCoinData.signed_change_rate.startsWith('-')){
+                      this.textColor.color = this.blueColor.color;
+                    }
+                    else{
+                      this.textColor.color = '#ffffff';
+                    }
                   }
 
                   this.coins_list_data.push({market : this.coins[i].market, korean_name : this.coins[i].korean_name, english_name : this.coins[i].english_name, market_warning : this.coins[i].market_warning,
@@ -449,39 +579,12 @@ export default {
                   });
               }        
             }     
-          }
+          }       
+      },     
 
-          this.setClass();          
-      },
-
-      setClass : function (){
-
-        var tr_coin_list = document.getElementById('tbl_coin_list').childNodes;
-
-        for(var i=1; i<tr_coin_list.length; i++){
-            var td_signed_change_rate = tr_coin_list[i].childNodes[2];
-            var txt_signed_change_rate = td_signed_change_rate.textContent;
-            this.toggleColorStyle(tr_coin_list[i], txt_signed_change_rate);
-        }
-
-        var seletedCoinCurrentPrice = document.getElementById('price_now');
-        this.toggleColorStyle(seletedCoinCurrentPrice, this.selectedCoinData.signed_change_rate);
-      },
-
-      toggleColorStyle : function(element, signed_change_rate){
-          signed_change_rate = String(signed_change_rate);
-
-          if(signed_change_rate.startsWith('+')){  
-            element.style.color = '#B84042';
-          }
-          else if(signed_change_rate.startsWith('-')){
-            element.style.color = '#0C66C6';
-          }
-          else {
-            element.style.color = 'white';
-          }
-      },
-
+      /**
+       * 코인명 검색 입력박스 변경이벤트 
+       */
       search_onchange : function(){
         var search_value = document.getElementById('coin_search').value;
         var tr_coin_list = document.getElementById('tbl_coin_list').childNodes;
@@ -503,10 +606,33 @@ export default {
             }
         }
       },
+
+      /**
+       * 코인 리스트의 항목 클릭 이벤트. 현재 선택된 코인 정보 변경
+       */
+      coinItem_onclick : function(coin){
+        this.selectedCoinData.marketCode = coin.market;
+        this.selectedCoinData.korean_name = coin.korean_name;
+        this.selectedCoinData.currentPrice = coin.currentPrice;
+        this.selectedCoinData.signed_change_rate = coin.signed_change_rate;
+        this.selectedCoinData.signed_change_price = coin.signed_change_price;
+        this.selectedCoinData.high_price = coin.high_price;
+        this.selectedCoinData.low_price = coin.low_price;  
+        this.selectedCoinData.acc_trade_price_24h = coin.acc_trade_price_24h;
+        this.selectedCoinData.acc_trade_volume_24h = coin.acc_trade_volume_24h;
+
+        this.retrieve_candles_init();
+      },
+
+      /**
+       * 현재 선택된 코인의 분 캔들 200개를 가져와 세팅
+       */
       retrieve_candles_init : function(){
         axios.get('http://localhost:8080/minuteCandles/'+this.unit, {params: { market : this.selectedCoinData.marketCode, count : 200}})
                         .then((result) => {
                             
+                            this.series[0].data = [];
+
                             for(var i =0; i<result.data.length; i++){
                               var data = result.data[i];
                               var candle_date_time_kst = data.candle_date_time_kst;
@@ -526,6 +652,10 @@ export default {
                         }) 
 
       },
+
+      /**
+       * 현재 선택된 코인의 캔들을 받아옴
+       */
       retrieve_current_candle : function(){
 
           axios.get('http://localhost:8080/minuteCandles/'+this.unit, {params: { market : this.selectedCoinData.marketCode}})
